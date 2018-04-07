c47b0bad88d5c0bdee24893840203fde46ddffce
Rik <gitaarik@gmail.com>
2018-04-07 18:46:36 +0200

    initial commit

---
+++ flower.js
@@ -0,0 +1,117 @@
+var rads = 250
+var nodes = 6
+var maxLevel = 5
+var rings = [];
+var moved = false;
+var grow = 1;
+
+function setup() {
+
+    colorMode(HSB);
+
+    var canvas = createCanvas(windowWidth, windowHeight);
+    var startx = width/2;
+    var starty = height/2;
+
+    var addedCoords = []
+
+    rings = []
+    rings.push(new Circle(startx,starty,rads));
+
+    function makeRings(x, y, level) {
+
+        level = level | 0
+
+        for(var i = 0;i < nodes;i++){
+
+            let newX = x + rads/2 * cos(2 * PI * i / nodes);
+            let newY = y + rads/2 * sin(2 * PI * i / nodes);
+
+            var coords = newX + ',' + newY
+
+            if (addedCoords.indexOf(coords) == -1) {
+                rings.push(new Circle(newX,newY,rads));
+                addedCoords.push(coords)
+            }
+
+            if (level < maxLevel) {
+                makeRings(newX, newY, level + 1)
+            }
+
+        }
+
+    }
+
+    makeRings(startx, starty)
+    trip()
+    flip()
+
+}
+
+function flip() {
+    grow = Math.random() < .5
+    setTimeout(flip, 800)
+}
+
+function trip() {
+
+    if (grow) {
+        mouseY += 2
+        mouseX += 30
+    } else {
+        mouseY -= 2
+        if (mouseX > 0) {
+            mouseX -= 60
+        }
+    }
+
+    setTimeout(trip, 50)
+
+}
+
+function draw(){
+    background(0,0,0,0.08);
+
+    for(var i = 0;i<rings.length;i++){
+        rings[i].draw();
+    }
+
+}
+
+
+let intensity = 128
+
+function mouseWheel(event) {
+
+    rads -= event.delta
+
+    if (rads < 0) {
+        rads = 0
+    } else if (rads > 500) {
+        rads = 500
+    }
+
+    setup()
+
+}
+
+
+class Circle {
+
+    constructor(x, y, r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    draw() {
+        let color = 0;
+        moved = true;
+        color = map(mouseX,0,width,0,255);
+        stroke(color, 150, intensity);
+        noFill();
+        ellipse(this.x, this.y, mouseY / 2, mouseY / 2);
+        moved = false;
+    }
+
+}
